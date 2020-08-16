5bdf44519dbc4395f9d1e70e36bef03d1289a375
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2020-08-16 13:21:41 -0500

    Initial branch commit

---
+++ bin-int/Debug-linux-x86_64/ImGui/imgui.d
@@ -0,0 +1,10 @@
+bin-int/Debug-linux-x86_64/ImGui/imgui.o: imgui.cpp imgui.h imconfig.h \
+ imgui_internal.h imstb_textedit.h
+
+imgui.h:
+
+imconfig.h:
+
+imgui_internal.h:
+
+imstb_textedit.h:
---
+++ premake5.lua
@@ -0,0 +1,31 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++11"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    
+    files {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
+
+filter "toolset:gcc"
+    buildoptions "-fPIC"
+
+filter "system:windows"
+    systemversion "latest"
+    cppdialect "C++17"
+    staticruntime "On"
+
+filter { "system:windows", "configurations:Release" }
+    buildoptions "/MT"
